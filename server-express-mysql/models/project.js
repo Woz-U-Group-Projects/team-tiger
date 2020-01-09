5195f0e7d912acbcb442d92351bab54a92ebd8c2
Landon DePaulo <52732062+ldp1984@users.noreply.github.com>
2020-01-09 14:18:36 -0800

    edited project.js

--- server-express-mysql/models/project.js
+++ server-express-mysql/models/project.js
@@ -1,31 +1,28 @@
-module.exports = function(sequelize, DataTypes) {
+'use strict';
+module.exports = (sequelize, DataTypes) => {
   var users = sequelize.define(
-    'users', 
+    'users',
     {
-    user_id: {
-      type: DataTypes.INTEGER(5).UNSIGNED,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    first_name: {
-      type: DataTypes.STRING(45),
-      allowNull: false,
-    last_name: {
-      type: DataTypes.STRING(45),
-      allowNull: false,
-    email_address: {
-      type: DataTypes.STRING(45),
-      unique: false,
-    },
-     Username: {
+      UserId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      FirstName: DataTypes.STRING,
+      LastName: DataTypes.STRING,
+      Email: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      Username: {
         type: DataTypes.STRING,
         unique: true
       },
       Password: DataTypes.STRING,
       createdAt: DataTypes.DATE,
       updatedAt: DataTypes.DATE
-  },  
+    },
     {}
   );
 
