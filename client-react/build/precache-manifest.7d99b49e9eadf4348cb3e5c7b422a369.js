aa18e18d90c5325c96cc11fafa31348b2788cd72
dj1126 <38404555+dj1126@users.noreply.github.com>
2020-02-01 01:21:08 -0800

    first commit

---
+++ client-react/build/precache-manifest.7d99b49e9eadf4348cb3e5c7b422a369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f4da3c22106c10cc26f193ed53c9ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97df152f2a96d62dead2",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "79c2e7c9d9f52fe0f46d",
+    "url": "/static/js/2.70b0bdbe.chunk.js"
+  },
+  {
+    "revision": "97df152f2a96d62dead2",
+    "url": "/static/js/main.bf52c30e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,13 +1,47 @@
-import React from "react";
-import Project from "./Project";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Project />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments: [],
+      loading: false
+    };
+  }
+
+  render() {
+    const loadingSpin = this.state.loading ? "App-logo Spin" : "App-logo";
+    return (
+      <div className="App container bg-light shadow">
+        <header className="App-header">
+          
+          <h1 className="App-title">
+            Leave a comment: <input type="text" name="comment"></input>
+            <span className="px-2" role="img" aria-label="Chat">
+              💬
+            </span>
+            <input type="submit"></input>
+          </h1>
+        </header>
+
+        <div className="row">
+          <div className="col-4  pt-3 border-right">
+            <div className="comment-box">
+
+            </div>
+            <textarea rows="4" cols="50" name="comment" form="usrform">
+How did you enjoy Tiger eats?</textarea>
+            {/* Comment Form Component */}
+          </div>
+          <div className="col-8  pt-3 bg-white">
+            {/* Comment List Component */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
